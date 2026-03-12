dedaa07ee0c31e923e7c4813b8898a07b3ea413d
gamaerry <gamaerry@gmail.com>
2026-03-12 13:56:38 -0600

    avance de la mision 2 del examen

---
+++ proyecto_numpy/examen/mision2.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+mitad1 = cv2.imread('m2_mitad1.png')
+mitad2 = cv2.imread('m2_mitad2.png')
+
+nuevo_lienzo = np.zeros((400, 400), dtype=np.uint8)
+
+nuevo_lienzo
+
+# 1. Crea lienzo de 400x400
+# 2. Traslada la mitad 1 y pégala
+# 3. Rota la mitad 2, trasládala y pégala
